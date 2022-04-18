17c415708210d40273a38004090692308fae23d0
Andrew Kang <andrewkang1106@gmail.com>
2022-04-18 18:08:42 -0400

    edit README.md

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -8,7 +8,7 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
-            /*
+            /* Initial basic implementation that esablishes connection and greets user, in this case me.
             var input = new HelloRequest { Name = "Andrew" };
             var channel = GrpcChannel.ForAddress("https://localhost:7042");
             var client = new Greeter.GreeterClient(channel);
